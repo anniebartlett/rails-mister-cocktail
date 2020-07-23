39363eaac9fa9d7f7c904cd6b2440f800cb0839e
Annie Bartlett <annie-bartlett@outlook.com>
2020-07-23 20:56:19 +0100

    completed full setup, adding styling to site

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["LET'S GET FIZZICAL!"],
+    typeSpeed: 70,
+    loop: false
+  });
+}
+
+export { loadDynamicBannerText };
