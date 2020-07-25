e1de2db7e29c892a4802397cdc687ec9806bc435
Annie Bartlett <annie-bartlett@outlook.com>
2020-07-25 15:31:06 +0100

    completed image upload

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -39,7 +39,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
   def redirect_to_cocktail
